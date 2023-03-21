ff5975c130fbf218d9f99bd68370b1fe21ffbc39
D_Creator <epidnugoairdrop@gmail.com>
2023-03-21 20:49:07 +0100

    display price

--- app.js
+++ app.js
@@ -9,35 +9,35 @@ const publicroutes = require("./routes/publicroutes")
 const app=express()
 fs = require("fs")
 const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const flash = require('connect-flash')
+const path = require('path')
 const cors = require('cors');
+
 
  
 
-app.use(cors({
-  origin: '*'
-}));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cookieParser())
-// app.use(session({ secret:process.env.SESSION_KEY }));
-app.use(flash());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: '*'
+}));
 
+// app.use(express.static(__dirname + "/dist"))
+// app.use('/static', express.static(__dirname + '/dist'))
+// var indexPage = fs.readFileSync(__dirname + '/dist/index.html', 'utf8')
 
-
-
-app.use(express.static(__dirname + "/dist"))
-app.use('/static', express.static(__dirname + '/dist'))
-var indexPage = fs.readFileSync(__dirname + '/dist/index.html', 'utf8')
+app.use(express.static(path.resolve(__dirname, '../kasuwa-react/dist')))
 
 
 app.set('view engine', 'ejs');
+
 const port=process.env.PORT || 9000;
 
 
+
+
 const dbURI=process.env.KASUWA_dbURI
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
 mongoose.set('strictQuery', true);  
 mongoose.connect(dbURI, {useNewurlParser:true,useUnifiedTopology:true,})
@@ -48,14 +48,12 @@ mongoose.connect(dbURI, {useNewurlParser:true,useUnifiedTopology:true,})
 
 app.use('/public', publicroutes)
 app.use('/admin', adminroutes)
-
-
-app.get("*", function (req, res){
-  return res.send(indexPage)
-})
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../kasuwa-react/dist', 'index.html'));
+});
 
 app.listen(port, () => { 
-  console.log(`node Kasuwa listening on port ${port}`)
+  console.log(`Kasuwa app listening on port ${port}`)
 })
 
 
